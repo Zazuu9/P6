ded0d943dec54afe8ec90e192b1b2ac3a8dcc4b6
Kevin Gouaille <kevingouaille8515@gmail.com>
2022-07-04 18:53:51 +0200

    Suppression d'un Thing

--- app.js
+++ app.js
@@ -31,9 +31,15 @@ app.post('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({error}))
 });
 
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id})
+    .then(() => res.status(201).json({message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({error}))
+})
+
 app.put('/api/stuff/:id', (req, res, next) => {
     Thing.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id})
-    .then((things) => res.status(200).json({message: 'Objet modifié !'}))
+    .then(things => res.status(200).json(things))
     .catch(error => res.status(404).json({error}))
 })
 
